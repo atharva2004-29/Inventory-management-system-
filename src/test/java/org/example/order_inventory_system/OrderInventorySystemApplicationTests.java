0ef40939881f5e23eae8413f70591587ea16dbdf
atharva2004-29 <atharvadhemare5@gmail.com>
2026-04-08 21:02:51 +0530

    chore: initial project skeleton

---
+++ src/test/java/org/example/order_inventory_system/OrderInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.order_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventorySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
