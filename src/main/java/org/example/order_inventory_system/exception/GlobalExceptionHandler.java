7032ff6327fa0ef43217dff46740f74d49a4dd02
chetnabendale13 <chetnabendale13@gmail.com>
2026-04-09 16:13:22 +0530

    added inventory module and updated config

--- src/main/java/org/example/order_inventory_system/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/order_inventory_system/exception/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package org.example.order_inventory_system.exception;
 
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleRuntimeException(RuntimeException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleEntityNotFound(EntityNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", "Record not found: " + ex.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGenericException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", "Something went wrong: " + ex.getMessage());
+        return "error";
+    }
 }
